8ca36deff0221a17922f475524f87f5a5d413dc3
jordanEscobar <jordanescosoto@gmail.com>
2020-05-31 18:15:34 -0600

    Actualización de la base de datos

--- misasignaturas.sql
+++ misasignaturas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 31-05-2020 a las 06:59:03
+-- Tiempo de generación: 01-06-2020 a las 02:14:00
 -- Versión del servidor: 10.4.11-MariaDB
 -- Versión de PHP: 7.4.4
 
@@ -24,6 +24,19 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `alumno`
+--
+
+CREATE TABLE `alumno` (
+  `idalumno` int(11) NOT NULL,
+  `nombreA` varchar(70) NOT NULL,
+  `telefonoA` varchar(12) NOT NULL,
+  `generoA` varchar(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `asignaturas`
 --
 
@@ -31,20 +44,51 @@ CREATE TABLE `asignaturas` (
   `idasignaturas` int(11) NOT NULL,
   `codigoasignaturas` varchar(15) NOT NULL,
   `nombreasignatura` varchar(40) NOT NULL,
-  `nota` int(11) NOT NULL
+  `nota` int(11) NOT NULL,
+  `idalumno` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `asignaturas`
 --
 
-INSERT INTO `asignaturas` (`idasignaturas`, `codigoasignaturas`, `nombreasignatura`, `nota`) VALUES
-(1, 'COD200', 'Base de datos', 55),
-(2, 'COD100', 'Programacion', 85);
+INSERT INTO `asignaturas` (`idasignaturas`, `codigoasignaturas`, `nombreasignatura`, `nota`, `idalumno`) VALUES
+(1, 'COD200', 'Base de datos', 70, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipo_usuario`
+--
+
+CREATE TABLE `tipo_usuario` (
+  `idTipoAlumno` int(11) NOT NULL,
+  `TipoUsuario` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `idUsuario` int(11) NOT NULL,
+  `nombreU` varchar(20) NOT NULL,
+  `passwordU` text NOT NULL,
+  `idalumno` int(11) NOT NULL,
+  `idTipoUsuario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `alumno`
+--
+ALTER TABLE `alumno`
+  ADD PRIMARY KEY (`idalumno`);
 
 --
 -- Indices de la tabla `asignaturas`
@@ -53,14 +97,44 @@ ALTER TABLE `asignaturas`
   ADD PRIMARY KEY (`idasignaturas`);
 
 --
+-- Indices de la tabla `tipo_usuario`
+--
+ALTER TABLE `tipo_usuario`
+  ADD PRIMARY KEY (`idTipoAlumno`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`idUsuario`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
+
+--
+-- AUTO_INCREMENT de la tabla `alumno`
+--
+ALTER TABLE `alumno`
+  MODIFY `idalumno` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `asignaturas`
 --
 ALTER TABLE `asignaturas`
   MODIFY `idasignaturas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `tipo_usuario`
+--
+ALTER TABLE `tipo_usuario`
+  MODIFY `idTipoAlumno` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
